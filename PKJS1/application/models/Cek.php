237df007ce1fa96b40b1359aff13a1f0a3ff18af
Thiar Hasbiya <thiar.hasbiya@gmail.com>
2017-10-02 21:14:35 +0700

    fungsi knn

--- PKJS1/application/models/Cek.php
+++ PKJS1/application/models/Cek.php
@@ -6,4 +6,11 @@ class Cek extends CI_Model {
         $query = $this->db->get('datatraining');
         return $query->result_array();
     }
+
+    public function get_knn($x,$y,$z,$k)
+    {
+    	$query_statement = "select * from datatraining order by SQRT(POW(sumbuX-".$x.",2)+POW(sumbuY-".$y.",2)+POW(sumbuZ-".$z.",2)) limit ".$k;
+    	$query = $this->db->query($query_statement);
+    	return $query->result_array();
+    }
 }
